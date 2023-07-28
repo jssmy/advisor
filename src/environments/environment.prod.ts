26470e9e96b687b4ab2163d9f27a37e77120785b
jssmy <servidorlogico@gmail.com>
2023-07-28 15:22:23 -0500

    error config user

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import {GrantType} from "../app/commons/constants/grant-type";
 export const environment = {
   authToken: 'https://advisor-api.barbacode.com/oauth/token',
   revokeToken: 'https://advisor-api.barbacode.com/api/v1/access/token',
-  authUserConfig: 'https://advisor-api.barbacode.com//api/v1/user/config',
+  authUserConfig: 'https://advisor-api.barbacode.com/api/v1/user/config',
   client_id: '99be762b-6c35-455a-84e2-6691d44359b9',
   client_secret: '3cgEECpj6y6xYgsrqCq6Ga5U0ib8GO69reOilUfk',
   grant_type: 'password' as GrantType
